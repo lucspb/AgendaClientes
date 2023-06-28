96a72534825c6a535e4923710c0384ce97ea7521
Lucas Estanislau <lucasestanislau@hotmail.com>
2023-06-27 21:21:54 -0300

    validator do formulario

--- agenda/src/main/webapp/scripts/validator.js
+++ agenda/src/main/webapp/scripts/validator.js
@@ -1,3 +1,21 @@
-/**
- * 
- */
+function validar(){
+	let nome = frmCliente.nome.value;
+	let fone = frmCliente.fone.value;
+	let endereco = frmCliente.endereco.value;
+	if(nome === ""){
+		alert("Preencha o campo Nome");
+		frmCliente.nome.focus();
+		return false;
+	} else if(fone === ""){
+		alert("Preencha o campo Fone");
+		frmCliente.fone.focus();
+		return false;
+	} else if(endereco === ""){
+		alert("Preencha o campo Endereco");
+		frmCliente.endereco.focus();
+		return false;
+	} else{
+		document.forms["frmCliente"].submit();
+	}
+}
+
